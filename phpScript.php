4496dcbb45857c7515c311cb5a56d401e5de241d
Tshilidzi Rambuda <46813734+vnashBio@users.noreply.github.com>
2020-10-19 17:18:59 +0200

    include file

--- phpScript.php
+++ phpScript.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once './vendor/autoload.php';
+require_once 'autoload.php';
 
 use FormGuide\Handlx\FormHandler;
 
